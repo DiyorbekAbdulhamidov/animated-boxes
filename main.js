c3184c303fe23c71192e9fee7a2a2aad5dda9a11
DiyorbekAbdulhamidov <drkoffical001@gmail.com>
2023-04-12 23:15:45 +0500

    js and style

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
 const dvd = document.querySelector('.dvdBox');
+const count = document.querySelector('.count')
 let topC = 0;
 let bottomC = 0;
 
+function randomHex(){
+    return '#' + Math.random().toString(16).substring(2,8)
+}
+
 setInterval(() => {
-    topC +=5
-    bottomC +=5;
+    topC ++;
+    bottomC ++;
 
     dvd.style.top = `${topC}px`;
     dvd.style.left = `${bottomC}px`;
 
-}, 40);
+    if(dvd.style.top == '700px'){
+        topC = 200;
+        bottomC = 700;
+        dvd.style.background = `${randomHex()}`
+        count.textContent = +count.textContent + 1;
+    }
+
+}, 0.2);
+
