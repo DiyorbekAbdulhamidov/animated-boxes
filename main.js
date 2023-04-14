58ac0c29157b849f324f09b9edc192608e94d46e
DiyorbekAbdulhamidov <drkoffical001@gmail.com>
2023-04-14 23:17:04 +0500

    js

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
 let random = Math.floor(Math.random() * 5) + 1;
 
-
 function randomHex() {
   return '#' + Math.random().toString(16).substring(2, 8)
 }
 
 for (let i = 0; i < random; i++) {
   let logo = document.createElement('div');
-  logo.className = 'dvd-logo';
-  let frag = document.createDocumentfragagment();
-  frag.appendChild(logo);
-  document.body.appendChild(frag);
+  logo.classList.add('dvd-logo');
+  let fr = document.createDocumentFragment();
+  fr.appendChild(logo);
+  document.body.appendChild(fr);
 
   let maxX = window.innerWidth - logo.offsetWidth;
   let maxY = window.innerHeight - logo.offsetHeight;
@@ -26,11 +25,13 @@ for (let i = 0; i < random; i++) {
     maxY = window.innerHeight - logo.offsetHeight;
     x += dx;
     y += dy;
+    
     if (x < 0 || x > maxX) {
       logo.style.background = randomHex();
       logo.textContent = +logo.textContent + 1;
       dx = -dx;
     }
+    
     if (y < 0 || y > maxY) {
       logo.textContent = +logo.textContent + 1;
       logo.style.background = randomHex()
@@ -38,7 +39,7 @@ for (let i = 0; i < random; i++) {
     }
     logo.style.left = x + 'px';
     logo.style.top = y + 'px';
-    requestAnimationfragame(animate);
+    requestAnimationFrame(animate);
   }
 
   animate();
